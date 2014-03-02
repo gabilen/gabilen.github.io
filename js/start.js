4527ba3be7c6436f46ff7306134b6030f825022f
hollote <ukrainetsvik@gmail.com>
2014-03-02 23:28:22 +0200

    fixed start eventManager

--- js/start.js
+++ js/start.js
@@ -6,14 +6,5 @@ require([
         'eventManager'
     ],function(init,EventManager){
         init();
-        var event1 = new EventManager();
-        event1.on("test",function(){console.log("Fuu4")});
-        event1.on("test",function(){console.log("Fuu2")});
-        event1.on("test",function(){console.log("Fuu1")});
-        event1.on("test",function(){console.log("Fuu3")});
-        event1.trigger("test");
-        event1.off("test",function(){console.log("Fuu3")});
-        
-        console.log(event1.listeners);
     });
 });
